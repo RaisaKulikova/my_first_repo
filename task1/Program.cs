2a0f694021eb67c97931eaddb1844135d2f64962
Raisa <raisaaaa@bk.ru>
2024-04-11 19:58:03 +0300

    add some variables task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+
+int a = 100;
+Console.WriteLine(a);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
